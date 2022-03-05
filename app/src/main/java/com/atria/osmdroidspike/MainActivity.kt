f17f05851f10687bd521c796ed6c5c142a692a61
Atria <atriasft@gmail.com>
2022-03-06 01:52:07 +0900

    set padding

--- app/src/main/java/com/atria/osmdroidspike/MainActivity.kt
+++ app/src/main/java/com/atria/osmdroidspike/MainActivity.kt
@@ -12,11 +12,15 @@ import androidx.preference.PreferenceManager
 
 import org.osmdroid.config.Configuration.*
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.BoundingBox
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
 import org.osmdroid.views.overlay.OverlayItem
 import org.osmdroid.views.overlay.Polyline
+import java.lang.Double.max
+import java.lang.Double.min
+import kotlin.math.abs
 
 
 class MainActivity : AppCompatActivity() {
@@ -34,20 +38,22 @@ class MainActivity : AppCompatActivity() {
         points = mutableListOf()
         map = findViewById(R.id.map)
         map.setTileSource(TileSourceFactory.MAPNIK)
+        map.minZoomLevel = 3.0
+        map.maxZoomLevel = 15.0
         val mapController = map.controller
-        mapController.setZoom(9.5)
+        mapController.setZoom(9.0)
         mapController.setCenter(miraiDai)
         map.setMultiTouchControls(true)//マップの2本指でのピンチアウト
         map.setLayerType(View.LAYER_TYPE_SOFTWARE,null)
 
-        addMarker(otaruEki)
-        addMarker(miraiDai)
         drawLine()
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener{
             val latitude = findViewById<EditText>(R.id.editText1).text.toString().toDouble()
             val longitude = findViewById<EditText>(R.id.editText2).text.toString().toDouble()
             addMarker(GeoPoint(latitude,longitude))
+            val area = getArea()
+            map.zoomToBoundingBox(area,true)
             drawLine()
         }
     }
@@ -64,6 +70,24 @@ class MainActivity : AppCompatActivity() {
         line.setPoints(points)
         line.outlinePaint.color = 0xAA900F42.toInt()
         map.overlays.add(line)
+    }
+    private fun getArea() : BoundingBox{
+        var north : Double = Double.MIN_VALUE
+        var south : Double = Double.MAX_VALUE
+        var east : Double = Double.MIN_VALUE
+        var west : Double = Double.MAX_VALUE
+        points.forEach{
+            val latitude = it.latitude
+            val longitude = it.longitude
+            north = max(latitude,north)
+            south = min(latitude,south)
+            west = min(longitude,west)
+            east = max(longitude,east)
+        }
+        println("n:$north e:$east s:$south w: $west")
+        val abs1 = (abs(north)-abs(south))/5
+        val abs2 = (abs(east)-abs(west))/5
+        return BoundingBox(north+abs1, east+abs2, south-abs1, west-abs2)
     }
     override fun onResume() {
         super.onResume()
